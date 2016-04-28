21a4ac87b5fa624d023b82039e0762333698ca42
matiasbastus <matiasbastus@gmail.com>
2016-04-28 14:33:17 -0300

    Instalacion OpenCart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/condordesign/admin/');
+define('HTTP_CATALOG', 'http://localhost/condordesign/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/condordesign/admin/');
+define('HTTPS_CATALOG', 'http://localhost/condordesign/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/condordesign/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/condordesign/system/');
+define('DIR_IMAGE', 'C:/wamp/www/condordesign/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/condordesign/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/condordesign/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/condordesign/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/condordesign/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/condordesign/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/condordesign/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/condordesign/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/condordesign/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/condordesign/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '185.28.21.241');
+define('DB_USERNAME', 'u430748566_condo');
+define('DB_PASSWORD', 'Metalmilitia1982');
+define('DB_DATABASE', 'u430748566_condo');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/condordesign/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/condordesign/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/condordesign/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/condordesign/system/');
+define('DIR_IMAGE', 'C:/wamp/www/condordesign/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/condordesign/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/condordesign/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/condordesign/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/condordesign/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/condordesign/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/condordesign/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/condordesign/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/condordesign/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '185.28.21.241');
+define('DB_USERNAME', 'u430748566_condo');
+define('DB_PASSWORD', 'Metalmilitia1982');
+define('DB_DATABASE', 'u430748566_condo');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
